6c6258b9856fe65ceb3066b9d12b62de7851d6aa
Mayara A. Moraes <salanderaraujo@gmail.com>
2022-09-26 15:39:08 -0300

    Adiciona requisito 2

---
+++ src/models/User.js
@@ -0,0 +1,17 @@
+const UserSchema = (sequelize, DataTypes) => {
+  const UserTable = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING
+  },
+    {
+      tableName: 'users',
+      underscored: true
+    });
+
+  return UserTable;
+}
+
+module.exports = UserSchema;
